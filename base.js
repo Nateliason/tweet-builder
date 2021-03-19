6457f71130bfa50858bb80ab7df7856e00b473a9
nateliason <nateliason@gmail.com>
2021-03-19 09:19:06 -0500

    first commit

---
+++ base.js
@@ -0,0 +1,61 @@
+const tweet1Count = document.querySelector('#tweet1Count');
+const tweet1 = document.querySelector('#tweetArea1');
+
+tweet1.addEventListener('input', function () {
+  tweet1Count.textContent = tweet1.textLength;
+});
+
+const saveButton = document.querySelector('#saveButton');
+
+saveButton.addEventListener('click', function () {
+  // Make the div for the saved tweet
+  const block = document.createElement('div');
+  block.className = 'savedTweet';
+
+  // Create a paragraph element in the div and add the tweet text
+  const para = document.createElement('p');
+  para.className = 'savedTweetText';
+  para.insertAdjacentText('beforeend', tweet1.value);
+
+  // Put the paragraph text in the div
+  block.appendChild(para);
+
+  // Make the Edit & Copy buttons
+  block.insertAdjacentHTML(
+    'beforeend',
+    ` <a href='#' onclick=editText(this)>Edit</a> <a href='#'>Copy</a>`
+  );
+
+  // Put the new div inside the savedTweets div
+  const saved = document.getElementById('savedTweets');
+  saved.appendChild(block);
+
+  // Reset the drafting box
+  tweet1.value = '';
+  tweet1Count.textContent = '0';
+});
+
+function editText(tweetText) {
+  const parent = tweetText.parentElement;
+  tweet1.value = parent.querySelector('p').textContent;
+  tweet1Count.textContent = tweet1.textLength;
+  parent.remove();
+}
+
+const fillWords = ['pretty', 'very'];
+
+fillWords.forEach((element) => console.log(element));
+
+// function checkFillerWords(tweetBody) {
+
+//   console.log(fillWords);
+
+//   fillWords.forEach(checkEachFillWord());
+//   function checkEachFillWord(value) {
+//     if (tweet1.value.includes(this) === true) {
+//       console.log(`don't use that word!`);
+//     } else {
+//       null;
+//     }
+//   }
+// }
---
+++ readme.md
@@ -0,0 +1,7 @@
+TODO
+[] Get the copy function working
+[] Add a button to open twitter in a new tab
+[] Directly tweet from the app? 
+[] Prevent input over 280 characters? 
+[] Recommended replacement words if you're being too verbose
+[] Have saved tweets persist or store locally 
